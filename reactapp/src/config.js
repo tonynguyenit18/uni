0555604e184e881651398f454e6e93386c344272
tonynguyenit18 <37351890+tonynguyenit18@users.noreply.github.com>
2019-06-16 04:44:38 +1000

    Update config.js

--- reactapp/src/config.js
+++ reactapp/src/config.js
@@ -1,3 +1,3 @@
 export const BASE_URL = "http://192.168.1.110:8080/";
-export const AWS_ACCESS_KEY_ID = "AKIAIUIRYNBO2ML4K7XQ";
-export const AWS_SECRET_ACCESS_KEY = "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";
+export const AWS_ACCESS_KEY_ID = "";
+export const AWS_SECRET_ACCESS_KEY = "";
