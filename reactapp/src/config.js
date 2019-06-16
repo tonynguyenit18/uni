385c60751e2a82a69ff64381c810dd64d330d394
tonynguyenit18 <37351890+tonynguyenit18@users.noreply.github.com>
2019-06-16 17:12:26 +1000

    Update config.js

--- reactapp/src/config.js
+++ reactapp/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "Your local IP address";
+export const BASE_URL = "http://Your_local_IP_address:8080/";
 export const AWS_ACCESS_KEY_ID = "Get it from me privately";
 export const AWS_SECRET_ACCESS_KEY = "Get it from me privately";
